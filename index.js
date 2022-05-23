6704f044c4107fd27c522c1bbbfaea6f7e21c79a
salmakhatun12345e <ummesalma0056@gmail.com>
2022-05-23 18:46:56 +0600

    mongodb added

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const express = require('express')
+const ObjectId = require('mongodb').ObjectId
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const cors = require("cors")
 require('dotenv').config();
 
@@ -8,6 +10,33 @@ const port = process.env.PORT || 5000
 // middleware
 app.use(cors());
 app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.9nqbm.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        await client.connect();
+        const toolsConnection = client.db("totalCare").collection("tools")
+
+        app.get('/get-tool', async (req, res) => {
+            const tools = await toolsConnection.find({}).toArray()
+            res.send(tools)
+        })
+        app.get('/get-tool/:id', async (req, res) => {
+            const id = req.params.id;
+            console.log(id)
+            const query = { _id: ObjectId(id) };
+            const result = await toolsConnection.findOne(query);
+            res.send(result);
+        });
+    }
+    finally {
+
+    }
+}
+run().catch(console.dir)
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
